e38ce5d121bfe91ccbd94341777de7225df65782
Seiry Yu <yuwenjie66@126.com>
2019-04-12 13:48:44 +0800

    +vue.config.js

---
+++ frontend/vue.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  configureWebpack: {
+    externals: {
+      //前者是内部名字
+      // echarts: 'echarts',
+      // "v-charts": 'v-charts'
+    }
+  },
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://g.gg/nuaa_guagua/backend/public/',
+        // target: 'https://gua.yuwenjie.cc/api',
+        pathRewrite: {
+          '^/api' : '/',
+        },
+        changeOrigin: true
+      }
+    }
+  },
+
+  productionSourceMap: false,
+
+  pwa: {
+    name: '小红帽',
+    themeColor: 'rgba(0,0,0,.87)'
+  }
+}
