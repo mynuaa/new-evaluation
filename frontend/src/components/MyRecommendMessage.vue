3aa01f241d7fa0efb5563c1de335e0de92d1e4e8
He <www.nuaaheyuhao@163.com>
2019-04-12 18:17:22 +0800

    增加了一个组件用于显示推荐的内容，目前很丑

---
+++ frontend/src/components/MyRecommendMessage.vue
@@ -0,0 +1,32 @@
+<!-- 各人推荐模块里的每一个推荐模块 -->
+<template>
+  <div class="all">
+        <div class="topic">
+            {{topic1}}
+        </div>
+        <div class="RightTop">
+            x
+        </div>
+        <div class="content">
+            {{content1}}
+        </div>    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyRecommendMessage',
+  props: {
+    msg: String,
+    content1:String,
+    topic1:String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.all{
+    height: 200px;
+}
+</style>
--- frontend/src/views/MyRecommendations.vue
+++ frontend/src/views/MyRecommendations.vue
@@ -1,22 +1,71 @@
+
 <template>
-  <div>
-    这里显示的是我的推荐的信息
-  </div>
+<v-container grid-list-xs>
+  <v-layout row wrap class="warning">
+    <v-flex md12>
+      <div id="waring_txt">
+      <v-card dark color="red" >
+        <v-card-text >请确保登录信息与教务处或研究生院网站完全一致。如出现票数投向陌生人的现象，请及时与校团委联系，并提供截图。</v-card-text>
+      </v-card>
+      </div>
+    </v-flex>
+  </v-layout>
+  <v-layout row wrap >
+    <v-flex md2>
+      <v-card dark color="blue">
+            <v-card-text>我的推荐</v-card-text>
+        </v-card>
+    </v-flex>
+  </v-layout>
+  <v-layout row wrap class="SumPoll">
+    <v-flex md2>
+      <v-card dark color="yellow">
+            <v-card-text>我的投票</v-card-text>
+        </v-card>
+    </v-flex>
+    </v-layout>
+    <MyRecommendMessage topic1="66" content1="6"></MyRecommendMessage>
+  <v-layout  row wrap class="Recommendation_message">
+    <v-flex md12 >
+      <v-card dark color="purple">
+            <v-card-text>你已投同学院{{same_yuan}}票，不同学院{{different_yuan}}票</v-card-text>
+        </v-card>
+    </v-flex>
+    <v-flex md12>
+      <v-card dark color="green">
+            <v-card-text>按照规则，总票数一共6票，投向其他学院票数不少于投向本学院票数的50%。</v-card-text>
+        </v-card>
+    </v-flex>
+    <v-flex md12>
+      <v-card dark color="orange">
+            <v-card-text>你还没给任何人投票哦~</v-card-text>
+        </v-card>
+    </v-flex>
+  </v-layout>
+</v-container>
 </template>
 
 <script>
+import MyRecommendMessage from '../components/MyRecommendMessage'
 export default {
   data(){
     return{
-      msg: 'ok~'
+      msg: 'ok~',
+      same_yuan:6,
+      different_yuan:2
     }
   },
   created(){
     // console.log(this)
+  },
+  components: {
+       MyRecommendMessage
   }
 }
 </script>
 
-<style scoped>
-
+<style lang="scss">
+.warning_txt{
+  height: 10px;
+}
 </style>
