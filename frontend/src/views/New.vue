0ed4d2aba088316a7042a2921078e7676ec27059
Unsy <unsy0420@163.com>
2019-04-12 19:57:10 +0800

    不知道发生了什么突然就好了,tinymce,自闭

--- frontend/src/views/New.vue
+++ frontend/src/views/New.vue
@@ -1,22 +1,27 @@
+
 <template>
-  <div>
-    新文章编辑器
+  <div id="demo">
+    <tinymce :height="300" v-model="content"/>
   </div>
 </template>
-
+ 
 <script>
+import Tinymce from '@tinymce/tinymce-vue';
 export default {
-  data(){
-    return{
-      msg: 'ok~'
-    }
+  name: "qrcode",
+  components: { 
+    'tinymce':Tinymce
   },
-  created(){
-    // console.log(this)
-  }
-}
+  data() {
+    return {
+      content: `<h1 style="text-align: center;">Welcome to the TinyMCE demo!</h1><p style="text-align: center; font-size: 15px;"><img title="TinyMCE Logo" src="//www.tinymce.com/images/glyph-tinymce@2x.png" alt="TinyMCE Logo" width="110" height="97" />`
+    };
+  },
+  methods: {},
+  mounted() {}
+};
 </script>
-
+ 
 <style scoped>
 
 </style>
