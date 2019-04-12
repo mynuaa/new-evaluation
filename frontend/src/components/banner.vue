209a990d03013c19105b7ec4e0dad7b1f4e95df7
SUER0_0 <563511736@qq.com>
2019-04-12 16:40:43 +0800

    banner+头图+选择框

---
+++ frontend/src/components/banner.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-alert :value="true" type="warning">{{msg}}</v-alert>
+</template>
+
+<script>
+export default {
+  name: "banner",
+  props: {
+    msg:String
+  }
+};
+</script>
+
+<style scoped>
+/* .container{
+    padding: 0;
+    margin: 0;
+} */
+.v-alert.warning {
+  border-radius: 4px;
+  background-color: rgb(240, 173, 78);
+  padding: 10px;
+  margin: 10px 0 15px 0;
+}
+.warning {
+  background-color: rgb(240, 173, 78);
+}
+</style>
+
+
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,13 +1,49 @@
 <template>
-  <HelloWorld />
+  <v-container>
+    <Banner msg="请确保登录信息与教务处或研究生院网站完全一致。如出现票数投向陌生人的现象，请及时与校团委联系，并提供截图。"/>
+    <v-img :src="require('../assets/banner.jpg')" height="480" class="grey darken-4"></v-img>
+    <v-layout class="filiteryuan">
+      <div v-for="(item, index) in items">
+        <v-btn color="error">{{item.content}}</v-btn>
+      </div>
+    </v-layout>
+    <HelloWorld/>
+  </v-container>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+import HelloWorld from "../components/HelloWorld";
+import Banner from "../components/banner";
 
-  export default {
-    components: {
-      HelloWorld
-    }
+export default {
+  data() {
+    return {
+      items: [
+        {
+          content: "航天",
+          yuan: 1
+        },
+        {
+          content: "航空",
+          yuan: 2
+        }
+      ]
+    };
+  },
+  components: {
+    HelloWorld,
+    Banner
   }
+};
 </script>
+
+<style scoped>
+.container {
+  padding: 0 100px;
+}
+.filiteryuan{
+  margin: 8px 0;
+  border: 1px solid red;
+  border-radius : 4px;
+}
+</style>
