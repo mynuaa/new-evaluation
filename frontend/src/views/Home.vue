e0d925c4aec0aa636725bf75559f985cbc4993af
Seiry Yu <yuwenjie66@126.com>
2019-04-12 13:50:21 +0800

    add vuetify

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,13 @@
 <template>
-  <div class="home">
-    <!-- 首页 -->
-    
-  </div>
+  <HelloWorld />
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  import HelloWorld from '../components/HelloWorld'
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  export default {
+    components: {
+      HelloWorld
+    }
   }
-}
 </script>
