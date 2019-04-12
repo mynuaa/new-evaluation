9917a01d922f3d9fc83ffdb03426341ef9a71201
Unsy <unsy0420@163.com>
2019-04-12 19:59:39 +0800

    change id ,name,content

--- frontend/src/views/New.vue
+++ frontend/src/views/New.vue
@@ -1,6 +1,6 @@
 
 <template>
-  <div id="demo">
+  <div id="editor-box">
     <tinymce :height="300" v-model="content"/>
   </div>
 </template>
@@ -8,13 +8,13 @@
 <script>
 import Tinymce from '@tinymce/tinymce-vue';
 export default {
-  name: "qrcode",
+  name: "new",
   components: { 
     'tinymce':Tinymce
   },
   data() {
     return {
-      content: `<h1 style="text-align: center;">Welcome to the TinyMCE demo!</h1><p style="text-align: center; font-size: 15px;"><img title="TinyMCE Logo" src="//www.tinymce.com/images/glyph-tinymce@2x.png" alt="TinyMCE Logo" width="110" height="97" />`
+      content: `请输入`
     };
   },
   methods: {},
